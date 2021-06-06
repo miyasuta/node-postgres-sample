d0086340c36b00bb2ca5272f1733785ad45c2e7c
Mio Yasutake <mio.fujita.01@gmail.com>
2021-06-06 17:29:54 +0900

    add approuter

--- srv/server.js
+++ srv/server.js
@@ -2,21 +2,69 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const JWTStrategy = require('@sap/xssec').JWTStrategy
+const xsenv = require('@sap/xsenv')
+const xssec = require('@sap/xssec')
 
 const dbConn = require('./db-conn')
-const dbOp = require('./db-op')
+const dbOp = require('./db-op');
+const xssecConfig = require('@sap/xssec/lib/xssec.config');
 
 var _db = undefined
 const app = express()
 
 app.use(bodyParser.json())
+passport.use(new JWTStrategy(xsenv.getServices({xsuaa:{tag:'xsuaa'}}).xsuaa));
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
 
 app.get('/', function (req, res) {
     res.send('Hello!')
 })
 
+function logJWT (req) {
+    var jwt = req.header('authorization');
+    if (!jwt) {
+        res.send('No JWT in Request - Call performed directly to App')
+        return
+    }
+    jwt = jwt.substring('Bearer '.length);
+    console.log(`JWT is: ${jwt}`)
+    xssec.createSecurityContext(jwt, xsenv.getServices({uaa: 'node-postgres-sample-uaa'}).uaa, function(error, securityContext) {
+        if (error) {
+            console.log('Security Context creation failed')
+            return            
+        }
+        var userInfo = {
+            logonName: securityContext.getLogonName(),
+            givenName: securityContext.getGivenName(),
+            familyName: securityContext.getFamilyName(),
+            emal: securityContext.getEmail()
+        }
+        console.log(`User Info retrieved successfully: ${JSON.stringify(userInfo)}`)
+
+        if (req.user) {
+            var myUser = JSON.stringify(req.user)
+            console.log(`user: ${JSON.stringify(req.user)}`)          
+        }
+    })
+}
+
 app.get('/products', function(req, res) {
+    //logJWT(req)
     dbOp.getAll(_db, res)
+
+    // console.log(`req.user: ${JSON.stringify(req.user)}`)
+    // console.log(`req.authInfo: ${JSON.stringify(req.authInfo)}`)
+    // console.log(`req.tokenInfo: ${JSON.stringify(req.tokenInfo)}`)
+    // console.log(`getLogonName: ${req.authInfo.getLogonName()}`)
+    // console.log(`TokenInfo-payload 1: ${JSON.stringify(req.authInfo.getTokenInfo().getPayload())}`)    
+    // console.log(`TokenInfo-payload 2: ${JSON.stringify(req.tokenInfo.getPayload())}`)   
+
+    // console.log('TokenInfo functions: ' + Object.getOwnPropertyNames(req.tokenInfo).filter(function (p) {
+    //     return typeof req.tokenInfo[p] === 'function';
+    // }));  
 })
 
 app.get('/products/:id', function(req, res) {
